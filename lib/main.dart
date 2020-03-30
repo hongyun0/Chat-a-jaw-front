ed6deee7cb43bc0fcbfddbc6660b6ce9de87d712
hongyun0 <hongyun0@naver.com>
2020-03-30 10:58:28 +0900

    Add initialization for device info and firebase messaging

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,18 @@
-import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:device_info/device_info.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+
+import 'deviceInfo.dart';
+
+final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+final String _myServerUrl = "http://10.0.2.2:8080";
 
 void main() => runApp(MyApp());
 
@@ -27,6 +40,94 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Map<String, dynamic> _deviceData = <String, dynamic>{};
+  String _token = "";
+
+  Future<void> initPlatformState() async {
+    Map<String, dynamic> deviceData;
+
+    try {
+      if (Platform.isAndroid) {
+        deviceData = readAndroidBuildData(await deviceInfoPlugin.androidInfo);
+      } else if (Platform.isIOS) {
+        deviceData = readIosDeviceInfo(await deviceInfoPlugin.iosInfo);
+      }
+    } on PlatformException {
+      deviceData = <String, dynamic>{
+        'Error:': 'Failed to get platform version.'
+      };
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _deviceData = deviceData;
+    });
+  }
+
+  void initFirebaseCloudMessagingListeners() {
+    if (Platform.isIOS) _iosPermission();
+
+    _firebaseMessaging.getToken().then((token) {
+      _token = token;
+      print('my token:' + token);
+
+      http.post("$_myServerUrl/notifications/tokens", body: {
+        "userId": _deviceData['deviceId'],
+        "token": token
+      }).then((response) {
+        if (response.statusCode == 200) {
+          print("토큰 등록 성공!");
+        } else {
+          print("토큰 등록 실패!");
+        }
+      });
+    });
+
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            content: ListTile(
+              title: Text(message["notification"]["title"]),
+              subtitle: Text(message["notification"]["body"]),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("OK"),
+                onPressed: () => Navigator.of(context).pop(),
+              )
+            ],
+          ),
+        );
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+      },
+    );
+  }
+
+  void _iosPermission() {
+    _firebaseMessaging.requestNotificationPermissions(
+        IosNotificationSettings(sound: true, badge: true, alert: true));
+    _firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {
+      print("Settings registered: $settings");
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+    initFirebaseCloudMessagingListeners();
+  }
+
   void _showToast(BuildContext context, String message) {
     final scaffold = Scaffold.of(context);
     scaffold.showSnackBar(
